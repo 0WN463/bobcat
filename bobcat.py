3599bcbdb7a306f85010870282ce6732362b195a
0WN463 <>
2022-08-09 00:22:37 +0800

    Autopep8

--- bobcat.py
+++ bobcat.py
@@ -26,8 +26,8 @@ else:
     CONFIG_FILE = os.path.join(CONFIG_DIR, '.bobcat.ini')
 
 SKIP_DIR = os.path.join(os.environ.get('XDG_STATE_HOME'), 'bobcat') \
-        if 'XDG_STATE_HOME' in os.environ \
-        else os.path.join(Path.home(), '.local', 'state', 'bobcat')
+    if 'XDG_STATE_HOME' in os.environ \
+    else os.path.join(Path.home(), '.local', 'state', 'bobcat')
 
 SKIP_FILE = os.path.join(SKIP_DIR, 'skipped')
 
@@ -44,43 +44,47 @@ config.read([DEFAULT_CONFIG_FILE, CONFIG_FILE])
 HOST = config['config']["host"]
 SOLUTION_FILE = config['config']['solution_file']
 CACHE_DIR = config['config']['cache']
-Path(CACHE_DIR).mkdir( parents=True, exist_ok=True )
+Path(CACHE_DIR).mkdir(parents=True, exist_ok=True)
 
 secret_conf = configparser.ConfigParser()
 secret_conf.read(os.path.join(CONFIG_DIR, '.secret.ini'))
 LANGUAGE_CONF = config["languages"]
 LANGUAGE_CONF = {k: literal_eval(v) for k, v in LANGUAGE_CONF.items()}
 
+
 @dataclass
 class Language:
-    name: str # Sent to Kattis to identify language
+    name: str  # Sent to Kattis to identify language
     ext: str
     build_cmd: str
     run_cmd: str
 
+
 LANGUAGES = [
-            Language(
-                "Python 3", 
-                ".py", 
-                LANGUAGE_CONF['python']['build'],
-                LANGUAGE_CONF['python']['exec'],
-            ),
-            Language(
-                "Haskell", 
-                ".hs", 
-                LANGUAGE_CONF['haskell']['build'],
-                LANGUAGE_CONF['haskell']['exec'],
-                ),
-            Language(
-                "C++", 
-                ".cpp", 
-                LANGUAGE_CONF['c++']['build'],
-                LANGUAGE_CONF['c++']['exec'],
-                )
-        ]
+    Language(
+        "Python 3",
+        ".py",
+        LANGUAGE_CONF['python']['build'],
+        LANGUAGE_CONF['python']['exec'],
+    ),
+    Language(
+        "Haskell",
+        ".hs",
+        LANGUAGE_CONF['haskell']['build'],
+        LANGUAGE_CONF['haskell']['exec'],
+    ),
+    Language(
+        "C++",
+        ".cpp",
+        LANGUAGE_CONF['c++']['build'],
+        LANGUAGE_CONF['c++']['exec'],
+    )
+]
+
 
 class AuthError(Exception):
     pass
+
 
 def login(username: str, password: str):
     LOGIN_URL = urllib.parse.urljoin(HOST, 'login')
@@ -101,30 +105,36 @@ class Problem:
     path: str
     difficulty: str
 
-def get_lang(file: str, languages:list[Language]=LANGUAGES) -> Language | None:
+
+def get_lang(file: str, languages: list[Language] = LANGUAGES) -> Language | None:
     ext = Path(file).suffix
     lang = next((lang for lang in languages if lang.ext == ext), None)
     return lang
 
+
 def get_probs(s) -> list[Problem]:
-    PROBLEM_LIST_URL = urllib.parse.urljoin(HOST, "/problems?order=%2Bdifficulty_category&show_solved=off&show_tried=on&show_untried=on")
+    PROBLEM_LIST_URL = urllib.parse.urljoin(
+        HOST, "/problems?order=%2Bdifficulty_category&show_solved=off&show_tried=on&show_untried=on")
     res = s.get(PROBLEM_LIST_URL)
     soup = BeautifulSoup(res.text, features='lxml')
 
     trs = [tr for tr in soup.table.tbody.find_all('tr')]
 
-    return [Problem(title=tr.td.text, 
-        path=tr.td.a['href'], 
-        difficulty=tr.findAll('td')[6].span.text) 
-        for tr in trs]
+    return [Problem(title=tr.td.text,
+                    path=tr.td.a['href'],
+                    difficulty=tr.findAll('td')[6].span.text)
+            for tr in trs]
+
 
 def download_samples(s, path: str, save_to=CACHE_DIR) -> None:
     shutil.rmtree(save_to)
-    Path(save_to).mkdir( parents=True, exist_ok=True )
-    SAMPLE_URL = urllib.parse.urljoin(HOST, f"{path}/file/statement/samples.zip")
+    Path(save_to).mkdir(parents=True, exist_ok=True)
+    SAMPLE_URL = urllib.parse.urljoin(
+        HOST, f"{path}/file/statement/samples.zip")
     r = s.get(SAMPLE_URL, stream=True)
     with zipfile.ZipFile(io.BytesIO(r.content)) as z:
         z.extractall(save_to)
+
 
 def get_prob(s, path: str):
     r = s.get(f"https://open.kattis.com{path}")
@@ -137,6 +147,7 @@ def get_prob(s, path: str):
         p.replace_with(re.sub(r'\s+', ' ', p.text))
     return body, samples
 
+
 def print_sample(sample, i: int):
     sample.tr.extract()
     print(f"Input {i}")
@@ -145,6 +156,7 @@ def print_sample(sample, i: int):
     print(f"Output {i}")
     print(sample.tr.td.extract().text.strip())
     print()
+
 
 def submit(s, problem_path, source_file) -> int:
     with open(source_file, 'r') as f:
@@ -158,10 +170,11 @@ def submit(s, problem_path, source_file) -> int:
         return 0
 
     prob = problem_path.replace('/problems/', '')
-    code_file = {"code": source, "filename": file_name, "id": 0, "session": None}
-    data = {"files": [code_file], 
-            "language": language.name, 
-            "problem": prob, 
+    code_file = {"code": source, "filename": file_name,
+                 "id": 0, "session": None}
+    data = {"files": [code_file],
+            "language": language.name,
+            "problem": prob,
             "mainclass": "",
             "submit": True,
             }
@@ -188,12 +201,15 @@ def local_run(solution_file=SOLUTION_FILE, test_case_dir=CACHE_DIR):
         out_file = file.replace("in", "out")
         ans_file = file.replace("in", "ans")
 
-        build_cmd = lang.build_cmd.format(sol=solution_file, cache_dir=test_case_dir)
+        build_cmd = lang.build_cmd.format(
+            sol=solution_file, cache_dir=test_case_dir)
         subprocess.Popen(build_cmd, shell=True, stdout=subprocess.PIPE).wait()
 
-        run_cmd = lang.run_cmd.format(source_file=solution_file, cache_dir=test_case_dir)
+        run_cmd = lang.run_cmd.format(
+            source_file=solution_file, cache_dir=test_case_dir)
         run_cmd = f'{run_cmd} < {file}'
-        out = subprocess.Popen(run_cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('ascii')
+        out = subprocess.Popen(
+            run_cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('ascii')
 
         print(f"Input: ")
 
@@ -202,7 +218,6 @@ def local_run(solution_file=SOLUTION_FILE, test_case_dir=CACHE_DIR):
 
         print("Output: ")
         print(out)
-
 
 
 def local_test(solution_file=SOLUTION_FILE, test_case_dir=CACHE_DIR) -> bool:
@@ -218,14 +233,17 @@ def local_test(solution_file=SOLUTION_FILE, test_case_dir=CACHE_DIR) -> bool:
         out_file = file.replace("in", "out")
         ans_file = file.replace("in", "ans")
 
-        build_cmd = lang.build_cmd.format(sol=solution_file, cache_dir=test_case_dir)
+        build_cmd = lang.build_cmd.format(
+            sol=solution_file, cache_dir=test_case_dir)
         subprocess.Popen(build_cmd, shell=True, stdout=subprocess.PIPE).wait()
 
-        run_cmd = lang.run_cmd.format(source_file=solution_file, cache_dir=test_case_dir)
+        run_cmd = lang.run_cmd.format(
+            source_file=solution_file, cache_dir=test_case_dir)
         diff_cmd = f'{run_cmd} < {file} > {out_file}'
         subprocess.Popen(diff_cmd, shell=True, stdout=subprocess.PIPE).wait()
 
-        diff = subprocess.Popen(f'diff --unified {ans_file} {out_file}', shell=True, stdout=subprocess.PIPE).stdout.read().decode('ascii')
+        diff = subprocess.Popen(f'diff --unified {ans_file} {out_file}',
+                                shell=True, stdout=subprocess.PIPE).stdout.read().decode('ascii')
 
         if diff:
             print(f"Solution produces different output for {file}")
@@ -239,20 +257,23 @@ def local_test(solution_file=SOLUTION_FILE, test_case_dir=CACHE_DIR) -> bool:
             is_correct = False
 
     return is_correct
-    
+
 
 def get_result(s, submission_id: int) -> (str, str):
     r = s.get(f'https://open.kattis.com/submissions/{submission_id}')
     soup = BeautifulSoup(r.text, features='lxml')
     result = soup.find('div', class_='status').text
-    test_cases = soup.find('div', class_='horizontal_item').find_all(text=True)[0]
+    test_cases = soup.find(
+        'div', class_='horizontal_item').find_all(text=True)[0]
     return result, test_cases
 
+
 def on_exit():
-    Path(SKIP_DIR).mkdir( parents=True, exist_ok=True )
+    Path(SKIP_DIR).mkdir(parents=True, exist_ok=True)
 
     with open(SKIP_FILE, 'w+') as f:
         f.write("\n".join(skipped_questions))
+
 
 if __name__ == '__main__':
     def show_prob(prob):
@@ -266,16 +287,17 @@ if __name__ == '__main__':
         for i, sample in enumerate(samples, start=1):
             print_sample(sample, i)
 
-
-    has_cred = secret_conf.has_section('credentials') and 'user' in secret_conf['credentials'] and 'password' in secret_conf['credentials']
+    has_cred = secret_conf.has_section(
+        'credentials') and 'user' in secret_conf['credentials'] and 'password' in secret_conf['credentials']
     user = secret_conf['credentials']['user'] if has_cred else input("User: ")
-    password = secret_conf['credentials']['password'] if has_cred else getpass.getpass()  
+    password = secret_conf['credentials']['password'] if has_cred else getpass.getpass(
+    )
 
     s = login(user, password)
     probs = get_probs(s)
 
     index = 0
-    probs = [ p for p in probs if p.path not in skipped_questions ]
+    probs = [p for p in probs if p.path not in skipped_questions]
     prob = probs[index]
     show_prob(prob)
 
@@ -287,7 +309,7 @@ if __name__ == '__main__':
             exit()
         if key.upper() in ['>', 'SKIP']:
             skipped_questions.append(prob.path)
-            probs = [ p for p in probs if p.path not in skipped_questions ]
+            probs = [p for p in probs if p.path not in skipped_questions]
             prob = probs[index]
             show_prob(prob)
             print(skipped_questions)
@@ -323,13 +345,12 @@ if __name__ == '__main__':
 
             print(result)
         elif key.upper() == 'N':
-            index += 1 
+            index += 1
             prob = probs[index]
             show_prob(prob)
         elif key.upper() == 'P':
-            index = max(0, index - 1) 
+            index = max(0, index - 1)
             prob = probs[index]
             show_prob(prob)
         else:
             print(f'"{key}" is not a valid command')
-
